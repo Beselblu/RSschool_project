c6ff67beee9c67026af5514dc2a367461dc4cb18
Tsenev Denis <cenev.denis@yandex.ru>
2022-05-10 14:33:55 +0300

    Pipeline add

---
+++ src/RSschool_project/pipeline.py
@@ -0,0 +1,23 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.feature_selection import VarianceThreshold
+
+def create_pipeline(
+    use_stdndart_scaller: bool, use_minmax_scaller: bool, max_iter:int,
+    logreg_C: float, random_state: int
+) -> Pipeline:
+    pipeline_steps = []
+    if use_stdndart_scaller:
+        pipeline_steps.append(('scaller', StandardScaler()))
+    if use_minmax_scaller:
+        pipeline_steps.append(('scaller', MinMaxScaler()))
+    pipeline_steps.append(
+        (
+            'classifier',
+            LogisticRegression(
+                random_state=random_state, max_iter=max_iter, C=logreg_C
+            )
+        )
+    )
+    return Pipeline(steps=pipeline_steps)
